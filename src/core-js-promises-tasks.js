fcf1796d06c65b29210c10ac9055ef135bd973dd
90Viktoriya <viktoria.kileeva@gmail.com>
2024-04-06 18:33:59 +0000

    feat: add 3 tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,12 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const result = new Promise((resolve, reject) => {
+    if (number >= 0) resolve(number);
+    else reject();
+  });
+  return result;
 }
 
 /**
@@ -33,8 +37,14 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  const result = new Promise((resolve) => {
+    source.then(
+      () => resolve('success'),
+      () => resolve('fail')
+    );
+  });
+  return result;
 }
 
 /**
@@ -50,8 +60,13 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  const result = new Promise((resolve, reject) => {
+    Promise.any(promises)
+      .then((value) => resolve(value))
+      .catch(() => reject());
+  });
+  return result;
 }
 
 /**
